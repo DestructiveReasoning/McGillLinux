58cf9ef1764924af68796eba61d43629931c3de4
Harley Wiltzer <harley.wiltzer@trusty>
2015-10-02 10:45:49 -0400

    Fixing savant

---
+++ savant/destructive_reasoning.h
@@ -0,0 +1,18 @@
+#ifndef __DESTRUCTIVE_REASONING__
+#define __DESTRUCTIVE_REASONING__
+
+#undef main
+#include <vector>
+#include <math.h>
+#include <stdio.h>
+#include <cstdlib>
+#include <string>
+#include <sstream>
+#include <cmath>
+#include <complex>
+#include <complex.h>
+#include <time.h>
+#include <iostream>
+#include <fstream>
+
+#endif
